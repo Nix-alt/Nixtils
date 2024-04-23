0bfb65ec70ec887f049c6a1adca72ae738069fda
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-04-23 10:54:38 -0400

    .event

--- app/src/main/java/com/nix/nixtils/events/DamageHandler.java
+++ app/src/main/java/com/nix/nixtils/events/DamageHandler.java
@@ -2,10 +2,10 @@ package com.nix.nixtils.events;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import com.nix.nixtils.Nixtils;
-import org.bukkit.Event;
+import org.bukkit.event;
 import org.bukkit.Bukkit;
-import org.bukkit.Event.Listener;
-import org.bukkit.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.eventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
