f7345e6c1ee9c071c849f26898bc15bcdecda5f8
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-04-23 10:51:12 -0400

    should be declared in a file called DamageHandler

--- app/src/main/java/com/nix/nixtils/events/DamageHandler.java
+++ app/src/main/java/com/nix/nixtils/events/DamageHandler.java
@@ -9,14 +9,14 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 
-public class DamagerHandler implements Listener{
-    public DamagerHandler(Nixtils plugin){
+public class DamageHandler implements Listener{
+    public DamageHandler(Nixtils plugin){
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
     public void OnDamage(EntityDamageEvent event){
-        if (!(event.getEntity instanceof Player)){
+        if (!(event.getEntity() instanceof Player)){
             return;
         }
         float damage = event.getDamage();
