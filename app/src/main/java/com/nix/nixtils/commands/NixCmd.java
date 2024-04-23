4f6e6e8a6f5c7784263e9b5a41fd66f71c61caf0
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-04-23 08:26:28 -0400

    made my cmd in a function and not a class

--- app/src/main/java/com/nix/nixtils/commands/NixCmd.java
+++ app/src/main/java/com/nix/nixtils/commands/NixCmd.java
@@ -12,7 +12,7 @@ import org.bukkit.ChatColor;
 
 
 //Register command class
-public void NixCmd extends CommandExecutor(){
+public class NixCmd extends CommandExecutor(){
 
     //Boolean for when command is run
     public boolean onCommand(CommandSender sender, Command command, string label, string [] args){
