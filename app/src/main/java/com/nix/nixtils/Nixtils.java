92d77fbd851f4828de07d0094b22cf15254642fe
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-04-22 21:49:36 -0400

    .

--- app/src/main/java/com/nix/nixtils/Nixtils.java
+++ app/src/main/java/com/nix/nixtils/Nixtils.java
@@ -1,18 +1,17 @@
 package com.nix.nixtils;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Nixtils extends JavaPlugin {
 
     @Override
 	public void onEnable() {
-        getlogger().info("Enabled");
+        Bukkit.getlogger().info("Enabled");
     }
 
     @Override
     public void onDisable() {
-        getlogger().info("Disabled");
+        Bukkit.getlogger().info("Disabled");
     }
 }
