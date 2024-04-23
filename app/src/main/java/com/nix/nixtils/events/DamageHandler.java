8995b1abf03549ae07a7220a4686e3514f5bc92a
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-04-23 10:56:34 -0400

    e

--- app/src/main/java/com/nix/nixtils/events/DamageHandler.java
+++ app/src/main/java/com/nix/nixtils/events/DamageHandler.java
@@ -2,10 +2,9 @@ package com.nix.nixtils.events;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import com.nix.nixtils.Nixtils;
-import org.bukkit.event;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
-import org.bukkit.eventHandler;
+import org.bukkit.event.eventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
@@ -20,7 +19,7 @@ public class DamageHandler implements Listener{
         if (!(event.getEntity() instanceof Player)){
             return;
         }
-        float damage = event.getDamage();
+        double damage = event.getDamage();
         String name = event.getEntity().getDisplayName();
         getLogger().info(ChatColor.translateAlternateColorCodes('&', "&cA player &a(" + name + ")&c was damaged."));
         getLogger().info(ChatColor.translateAlternateColorCodes('&', "&cThey took " + damage + " damage."));
